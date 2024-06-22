e0a17339905ffb820c8954d84c1578e26649cdd3
Christian Caluya <christianlcaluya@iskolarngbayan.pup.edu.ph>
2024-06-22 15:13:10 +0800

    Add project files.

---
+++ PhoneBookData/Functions.cs
@@ -0,0 +1,7 @@
+﻿namespace PhoneBookData
+{
+    public class Functions
+    {
+
+    }
+}
---
+++ PhoneBookModels/Contacts.cs
@@ -0,0 +1,12 @@
+﻿namespace PhoneBookModels
+{
+    public class Contacts
+    {
+        public string c_name;
+        public string c_num;
+        public string c_email;
+        public string c_add;
+
+
+    }
+}
